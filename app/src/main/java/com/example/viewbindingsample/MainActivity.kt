619dac1e4c923d505d14db101954a8fff871ec7f
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-28 15:37:09 +0400

    feat(app): add 'MainActivity'

--- app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.textViewActivity.text = getString(R.string.hello_from_vb_activity)
     }
 }
